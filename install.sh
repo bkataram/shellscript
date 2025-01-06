b0798a074eb7fa0a1315f3170bcdd5484643986b
unknown <bkataram@example.com>
2025-01-06 19:30:03 +0530

    kl

--- install.sh
+++ install.sh
@@ -2,6 +2,24 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
- echo "ERROR yo dont have sudo access"
+  echo "ERROR yo dont have sudo access"
+  exit 1
  fi
- dnf install mysql -y
+dnf install mysql -y
+ if  [ $? -ne 0 ]
+  then
+   echo "INSATLLING  MY SQL FAILURE .."
+  exit 1
+ else
+  echo "INSTALLING  MY SQL SUCESS .."
+ fi
+ 
+ dnf install git -y
+
+  if  [ $? -ne 0 ]
+  then
+   echo "INSATLLING  GIT FAILURE .."
+  exit 1
+ else
+  echo "INSTALLING  GIT SUCESS .."
+ fi
